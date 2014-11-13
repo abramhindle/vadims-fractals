f0058b0828509cb8197bd1b7cfc7a53e323f7f4e
Abram Hindle <abram.hindle@softwareprocess.es>
2014-11-13 11:25:01 -0700

    render

--- mk50vid.sh
+++ mk50vid.sh
@@ -2,7 +2,7 @@
 SIZE=$1
 SPEED=$2
 DECAY=$3
-FILE="50pict-size${SIZE}-quartz-speed${SPEED}-decay${DECAY}-3-7s"
+FILE="50pict-size${SIZE}-speed${SPEED}-decay${DECAY}-3-7s"
 go run pict.go --speed=$SPEED --decay=$DECAY --size=$SIZE > $FILE.sco
 csound -o $FILE.wav sine.orc $FILE.sco
 #mplayer 50.mov --audiofile=$FILE.wav
